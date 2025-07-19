9753a082779d5fb8406015c51b266e99c1c11d9a
MarcelloCavazza <marcellocavazzaoliveira@gmail.com>
2025-07-19 20:26:43 -0300

    createing project files

---
+++ message-publisher/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using message_publisher.models;
+
+var customerTest = new Customer()
+{
+	Id = Guid.NewGuid(),
+	FullName = "Teste",
+	Email = "email@gmail.com",
+	BirthDate = DateTime.Now
+};
+
+
+Console.WriteLine(customerTest);
