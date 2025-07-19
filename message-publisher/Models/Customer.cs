b348915d395b3522d8d00678f00edd7cbfc0202b
MarcelloCavazza <marcellocavazzaoliveira@gmail.com>
2025-07-19 20:26:07 -0300

    created initial entities

---
+++ message-publisher/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿namespace message_publisher.models;
+
+public class Customer
+{
+	public required Guid Id { get; set; }
+
+	public required string Email { get; set; }
+
+	public required string FullName { get; set; }
+
+	public required DateTime BirthDate { get; set; }
+
+	public override string ToString()
+	{
+		return $"FullName: {FullName} - Email: {Email} - BirthDate: {BirthDate}";
+	}
+}
+    
